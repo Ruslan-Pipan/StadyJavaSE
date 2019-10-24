8f458097a69db083e2a33a6b2b8df488a5c4b769
Ruslan Pipan <rus.pipan@ukr.net>
2019-10-24 20:19:20 +0300

    Moved class DataList in SinglLinkedList

--- src/colectioms/list/singLinkedList/SinglLinkedList.java
+++ src/colectioms/list/singLinkedList/SinglLinkedList.java
@@ -10,6 +10,42 @@ public class SinglLinkedList  implements List  {
         this.quentati = -1;
     }
 
+    class DataList{
+        private int index;
+        private int date;
+        private DataList next;
+
+        DataList(int element){
+            this.date = element;
+            this.index = -1;
+        }
+
+        void setIndex(int index) {
+            this.index = index;
+        }
+
+        int getIndex() {
+            return index;
+        }
+
+        DataList getNext() {
+            return next;
+        }
+
+        void setDate(int date) {
+            this.date = date;
+        }
+
+        int getDate() {
+            return date;
+        }
+
+
+        void setNext(DataList next) {
+            this.next = next;
+        }
+    }
+
     @Override
     public boolean add(int element, int index) {
         DataList elemenyЕqualsIndex = lockingForIndex(index);
