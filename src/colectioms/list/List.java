ee42e41ed94f86d0f8aba1f77fe2b88951f1d352
Ruslan Pipan <rus.pipan@ukr.net>
2019-10-24 21:46:04 +0300

    Edined metod Push on add;

--- src/colectioms/list/List.java
+++ src/colectioms/list/List.java
@@ -3,12 +3,13 @@ package colectioms.list;
 import colectioms.Colection;
 
 
-public interface List extends Colection {
-    boolean add(int element, int index);
-    boolean set(int index, int elemeny);
-    int get(int E);
-    boolean isHas(int element);
+public interface List<E> extends Colection<E> {
+    boolean add(E element, int index);
+    boolean add(E element);
+    boolean set(int index, E elemeny);
+    E get(int index);
+    boolean isHas(E element);
     boolean remove(int index);
-    int indexOf(int element);
+    int indexOf(E element);
     void print();
 }
