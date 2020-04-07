eea388dfd90a8fd56ada23ed132c48c8b13c2db5
Ruslan Pipan <rus.pipan@ukr.net>
2020-04-07 20:39:21 +0300

    Change implement methods getNexElement, getPrevElement. Studied interface NavigableSet.

--- src/tasks/Set/TreSetTasks/Tasks.java
+++ src/tasks/Set/TreSetTasks/Tasks.java
@@ -1,13 +1,10 @@
 package tasks.Set.TreSetTasks;
 
-import java.util.Iterator;
-import java.util.Set;
-
-import java.util.TreeSet;
+import java.util.*;
 
 public class Tasks {
 
-    private Set<Integer> set = new TreeSet<>();
+    private NavigableSet<Integer> set = new TreeSet<>();
     private  Iterator<Integer> iterator;
 
     public Tasks() {
@@ -18,33 +15,11 @@ public class Tasks {
             set.add(i);
         }
     }
-
+    
     public Integer getNexElement(Integer elem){
-        iterator = set.iterator();
-        while (iterator.hasNext()){
-            Integer tmp = iterator.next();
-            if(tmp.equals(elem))
-                return iterator.next();
-        }
-         return null;
+         return set.higher(elem);
     }
     public  Set<Integer> getPrevElement(Integer elem){
-        iterator = set.iterator();
-        int counter = -1;
-        Set<Integer> newSet = new TreeSet<>();
-        while (iterator.hasNext()){
-            counter++;
-            Integer tmp = iterator.next();
-            if (tmp.equals(elem)) {
-                iterator = set.iterator();
-                while (counter != -1){
-                    tmp = iterator.next();
-                    newSet.add(tmp);
-                    counter--;
-                }
-                break;
-            }
-        }
-       return newSet;
+        return set.headSet(elem);
     }
 }
