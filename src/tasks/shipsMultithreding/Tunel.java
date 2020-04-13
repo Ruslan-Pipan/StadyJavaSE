1ffd4846f6be614ab5696a098df74e3ac8ce3a79
Ruslan Pipan <rus.pipan@ukr.net>
2020-04-13 16:08:09 +0300

    Tunel- have ships witch go to port. Made for multithreading

---
+++ src/tasks/shipsMultithreding/Tunel.java
@@ -0,0 +1,28 @@
+package tasks.shipsMultithreding;
+
+
+import java.util.Queue;
+
+public class Tunel implements Runnable{
+
+    private Queue<Ship> queue;
+    private Ship ships;
+
+    public Tunel(Ship ship){
+        this.queue = ship.getQueueShips();
+        this.ships = ship;
+    }
+
+
+    public synchronized Queue<Ship> getQueue() {
+        return queue;
+    }
+
+    @Override
+    public synchronized void run() {
+        if (queue.size() == 0)
+            queue = ships.getQueueShips();
+        if (queue.size() > 5)
+            notifyAll();
+    }
+}
